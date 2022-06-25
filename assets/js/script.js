8f5c0344834aa682e34dcea085f338641c8e39e8
violanerd <madalyne.cross@gmail.com>
2022-06-25 17:23:53 -0500

    added save functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 // hold the tasks
-var tasks = {};
+var tasks = [];
 
-// click save button, text entered into tasks 
-// tasks gets updated in local storage
+
 
 //task save was clicked
 $(".save-btn").on("click", function(){
-    var buttonRow = $(this).closest("div").attr('data-time');
     
-    console.log(buttonRow);
+    // get the hour and the activity
+    var div = $(this).closest("div");
+    var hour = div.attr('data-time');
+    var activity =div.children("textarea").val(); 
+    console.log(hour, activity);
+
+    // save in tasks array
+    tasks.push([hour, activity]);
+    console.log(tasks);
+    // tasks gets updated in local storage
+    saveTasks();
 });
-//get data id attribute fom div
-    //$("#buttonID").closest("[data-testing]")
+
 
 
 // display current date
@@ -20,3 +27,22 @@ $(document).ready(function(){
     $("#currentDay").text(date);
 });
 
+loadTasks();
+
+function loadTasks () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log(tasks);
+ //   for (var i=0; i<tasks.length; i++){
+ //       var hour = tasks[i][0];
+ //       var div = $("[data-time=").
+ //   }
+
+ //how to get the div with the hour information
+    // loop through the tasks
+    // use the hour to find the correct div
+    // set text to that div - same as activity? 
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
