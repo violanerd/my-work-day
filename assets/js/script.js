2c5b3e4fdb890064b0536821a7a786e7f7b88ff4
violanerd <madalyne.cross@gmail.com>
2022-06-25 12:02:16 -0500

    click event on save button- get the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// hold the tasks
+var tasks = {};
+
+// click save button, text entered into tasks 
+// tasks gets updated in local storage
+
+//task save was clicked
+$(".save-btn").on("click", function(){
+    var buttonRow = $(this).closest("div").attr('data-time');
+    
+    console.log(buttonRow);
+});
+//get data id attribute fom div
+    //$("#buttonID").closest("[data-testing]")
+
+
+// display current date
 var date = moment().format('dddd, MMMM Do');
 $(document).ready(function(){
     $("#currentDay").text(date);
